1a6da4690484f108d0a0da314938c7d3266d8718
Anish <anishmrzan@gmail.com>
2025-01-18 14:17:27 +0545

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from models.resnet import ResNet
 from utils.helper import load_image, class_names
 from optimizers.optimizers import get_optimizer
 from transforms.transforms import transform_train, transform_test
-from trainer.trainer import train_model, evaluate_model
+from trainer.trainer import train_model, evaluate_model, calculate_and_visualize_explanations
 import os
 import numpy as np
 from PIL import Image
@@ -18,21 +18,26 @@ MODEL_SAVE_PATH = 'best_resnet_model.pth'
 
 @st.cache_resource
 def load_model():
-    net = ResNet(num_classes=10).to(device)
-    if os.path.exists(MODEL_SAVE_PATH):
-         print("Loading pre-trained model...")
-         net.load_state_dict(torch.load(MODEL_SAVE_PATH))
-    else:
-        print("Training model from scratch...")
-        train_data = CIFAR10(root='./data', train=True, transform=transform_train, download=True)
-        test_data = CIFAR10(root='./data', train=False, transform=transform_test, download=True)
-        train_loader = DataLoader(train_data, batch_size=16, shuffle=True, num_workers=2)
-        test_loader = DataLoader(test_data, batch_size=16, shuffle=False, num_workers=2)
-        criterion = nn.CrossEntropyLoss()
-        optimizer = get_optimizer('SGD', net.parameters(), 0.002155517765181785)
-        net = train_model(net, train_loader, optimizer, criterion, num_epochs=30, device=device)
-        torch.save(net.state_dict(), MODEL_SAVE_PATH)
-    return net
+    try:
+        net = ResNet(num_classes=10).to(device)
+        if os.path.exists(MODEL_SAVE_PATH):
+            print("Loading pre-trained model...")
+            net.load_state_dict(torch.load(MODEL_SAVE_PATH))
+        else:
+            print("Training model from scratch...")
+            train_data = CIFAR10(root='./data', train=True, transform=transform_train, download=True)
+            test_data = CIFAR10(root='./data', train=False, transform=transform_test, download=True)
+            train_loader = DataLoader(train_data, batch_size=16, shuffle=True, num_workers=2)
+            test_loader = DataLoader(test_data, batch_size=16, shuffle=False, num_workers=2)
+            criterion = nn.CrossEntropyLoss()
+            optimizer = get_optimizer('SGD', net.parameters(), 0.002155517765181785)
+            net = train_model(net, train_loader, optimizer, criterion, num_epochs=30, device=device)
+            torch.save(net.state_dict(), MODEL_SAVE_PATH)
+            evaluate_model(net, test_loader, device=device)
+        return net
+    except Exception as e:
+        print(f"Error loading/training model: {e}")
+        return None
 
 
 def predict_and_explain(image, net, image_path):
@@ -42,6 +47,7 @@ def predict_and_explain(image, net, image_path):
     class_idx = predicted.item()
     
     st.write(f'Predicted class index: {class_idx}, Predicted class name: {class_names[class_idx]}')
+
 
     net_for_gradcam = ResNet(num_classes=10).to(device)
     net_for_gradcam.load_state_dict(net.state_dict())
@@ -56,8 +62,8 @@ def predict_and_explain(image, net, image_path):
     overlay = utils.gradcam.overlay_heatmap(original_image, heatmap)
 
     if overlay is not None:
-        overlay_image = Image.fromarray(overlay) 
-        st.image(overlay_image, caption='Grad-CAM Heatmap', use_column_width=True)
+      overlay_image = Image.fromarray(overlay)
+      st.image(overlay_image, caption='Grad-CAM Heatmap', use_column_width=True)
     else:
       st.write("Could not generate Grad-CAM Heatmap")
 
@@ -67,7 +73,7 @@ def main():
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     net = load_model()
-    if uploaded_file is not None:
+    if net is not None and uploaded_file is not None: #Check if the model has loaded correctly
         try:
           
           image_path = uploaded_file.name  
